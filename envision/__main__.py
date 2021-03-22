0a1ec2cd01dffedde1f2a78bd2d1ed2dcfc34565
Molly Pabst <52474395+mollypabst@users.noreply.github.com>
2021-03-22 10:04:31 -0400

    Add files via upload

--- envision/__main__.py
+++ envision/__main__.py
@@ -1,8 +1,7 @@
 from sys import argv
 from warnings import filterwarnings
-
 from pandas import set_option
-
+from datetime import datetime
 from envision import update, lighting, occupancy
 
 filterwarnings("ignore")
@@ -12,7 +11,7 @@ set_option("display.width", 0)
 output = {
     "lighting": lighting,
     "update": update,
-    "occupancy": occupancy,
+    "occupancy": occupancy(datetime.datetime.now(), datetime.datetime.now() - datetime.timedelta(hours = 1)),
 }[argv[1]]()
 
 if hasattr(output, "fillna"):
